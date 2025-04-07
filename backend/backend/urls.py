e6438d982b0df365a26d30dbdcc2893cf6838c63
Sourish Chandra <kisuke@PolishGirl.kali>
2025-04-08 02:20:09 +0530

    there was a mistake

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,6 +27,5 @@ urlpatterns = [
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('api/ticketbooking/', include('ticketbooking.urls')),
 
 ]
