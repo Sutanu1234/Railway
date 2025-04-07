0f052c261ce05439b173233ef380dfa58cc80f2f
Sourish Chandra <kisuke@PolishGirl.kali>
2025-04-08 02:18:52 +0530

    added the url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,9 +21,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('account.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/employee/', include('staff.urls')),
+    path('api/ticketbooking/', include('ticketbooking.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/ticketbooking/', include('ticketbooking.urls')),
+
 ]
